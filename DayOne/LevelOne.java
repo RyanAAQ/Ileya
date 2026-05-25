4837bea78bf4ae80276a7a98f642a4d7f170191a
Ryan <ariyoquadri4@gmail.com>
2026-05-25 19:46:08 +0100

    Phase Gate

---
+++ DayOne/LevelOne.java
@@ -0,0 +1,76 @@
+import java.util.Arrays;
+
+public class LevelOne{
+//LevelOne
+    public static int[][] evenAndOddNumbers(int[] numbers){
+        int oddCount = 0;
+        int evenCount = 0;
+
+        for (int number : numbers) {
+            if (number % 2 == 0) {;;;
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+}
+        int[] even = new int[evenCount];
+        int[] odd = new int[oddCount];
+
+        int odds = 0;
+        int evens = 0;
+
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                even[evens++] = number;
+            } else {
+                odd[odds++] = number;
+            }
+        }
+        return new int[][]{odd, even}; 
+    }
+    
+    public static boolean isPalindrome(int[] numbers){
+        int left  = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            if (numbers[left] != numbers[right])
+            return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+    public static int[] perfectSquare(int[] numbers){
+        int count = 0;
+        for (int number : numbers) {
+            int squareRoot = (int)Math.sqrt(number);
+            if (squareRoot * squareRoot == number){
+            count++;
+            }
+        }
+        int[] result = new int[count];
+        int index = 0;
+
+        for (int number : numbers) {
+            int squareRoot = (int)Math.sqrt(number);
+            if (squareRoot * squareRoot == number){
+            result[index++] = number;
+        }
+        }
+        return result;
+    }
+//LevelTwo    
+    public static int[] zeroToEnd(int[] numbers){
+        int[] result = new int[numbers.length];
+        int index = 0;
+
+        for (int number : numbers) {
+            if (number != 0) result[index++] = number;
+        }
+        return result;
+    }
+}
+
+
---
+++ DayOne/LevelOneTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LevelOneTest{
+
+    @Test
+    public void testEvenAndOddNumbersAreDisplayed() {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[][] expected = {{1, 3, 5, 7, 9}, {2, 4, 6 ,8, 10}};
+        int[][] actual = LevelOne.evenAndOddNumbers(numbers);
+        assertArrayEquals(expected, actual);
+    }
+    
+    @Test
+    public void testThatTheContentOfTheArrayIsAPalindrome(){
+        int [] numbers = {1, 2, 3, 2, 1};
+        boolean actual = LevelOne.isPalindrome(numbers);
+        boolean expected = true;
+        assertEquals(actual, expected);
+    }
+    
+    @Test
+    public void testThatTheContentOfTheArrayIsNotAPalindrome(){ 
+        int [] numbers = {1, 2, 3, 2, 1, 1, 2, 3};
+        boolean actual = LevelOne.isPalindrome(numbers);
+        boolean expected = false;
+        assertEquals(actual, expected);
+    }
+    
+    @Test
+    public void testThatTheFunctionReturnsAnArrayWithPerfectSquare(){
+        int[] numbers = {4, 7, 9, 10, 16, 18};
+        int[] expected = {4, 9, 16};
+        int[] actual = LevelOne.perfectSquare(numbers);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testThatTheZerosAreMovedTheEndOfTheArray(){
+        int[] numbers = {4, 5, 0, 4, 0, 2, 0, 0, 1};
+        int[] expected = {4, 5, 4, 2, 1, 0, 0, 0, 0};
+        int[] actual = LevelOne.zeroToEnd(numbers);
+        assertArrayEquals(expected, actual);
+    }
+}
+
